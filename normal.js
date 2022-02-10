8c7232f85168655419ae16ad27728fb62d8d0348
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-02-10 22:48:43 +0530

    added an answer preview

--- normal.js
+++ normal.js
@@ -14,16 +14,18 @@ button.addEventListener("click", count);
         document.getElementById('button').textContent = 'Submit';
         if (i < 20){
             var b = new Date();
-            //console.log(ax.getSeconds() + ax.getMinutes()*60 + ax.getHours()*3600);
             var ax = new Date();
+            console.log(ax.getSeconds() + ax.getMinutes()*60 + ax.getHours()*3600);
             var result = document.getElementById('result');
             var input = document.getElementById('input');
             if (parseInt(input.value) === questions[questions.length - 1 ]){
                 right += 1;
+                document.getElementById('answer').textContent = '';
                 result.textContent = 'Correct!';
             }else{
                 if (i !== 0){
-                    result.textContent = 'Wrong';
+                    result.textContent = 'Wrong.' ;
+                    document.getElementById('answer').textContent = `Correct Answer: ${String(questions[questions.length - 1])}`;
                 }
             };
             answered.push(input.value);
@@ -36,8 +38,8 @@ button.addEventListener("click", count);
             input.value = '';
             var question = document.getElementById('question');
             i += 1;
-            var x = Math.floor(Math.random()*30);
-            var y = Math.floor(Math.random()*30);
+            var x = Math.floor(Math.random()*29) + 1;
+            var y = Math.floor(Math.random()*29) + 1;
             var z = ['+','-','*','/'][Math.floor(Math.random()*3)];
             if (z === '+'){
                 last_questions.push(String(x) + ' + ' + String(y));
@@ -63,12 +65,19 @@ button.addEventListener("click", count);
             var input = document.getElementById('input');
             if (parseInt(input.value) === questions[questions.length - 1 ]){
                 right += 1;
+                document.getElementById('result').textContent = 'Correct!';
+            }else{
+                if (i !== 0){
+                    document.getElementById('result').textContent = `Wrong, Correct Answer: ${String(questions[questions.length - 1])}`;
+                }
             };
-            var result = document.getElementById('result');
+            input.value = ''
+            var answer = document.getElementById('answer');
             var end = new Date();
             var time_taken =  Math.round(end.getSeconds() + end.getMinutes()*60 + end.getHours()*3600 - (start.getSeconds() + start.getMinutes()*60 + start.getHours()*3600));
             //(b.getSeconds() + b.getMinutes()*60 + b.getHours()*3600 - a.getSeconds() - a.getMinutes()*60 - a.getHours()*3600)
-            result.textContent =  'Time taken: '+ String(time_taken) + ' seconds, Right answers: '  + right;
+            answer.textContent =  'Time taken: '+ String(time_taken) + ' seconds, Right answers: '  + right;
+            var button = document.getElementById('button');
             button.textContent = 'Redo';
         }else{
             window.location.reload();
